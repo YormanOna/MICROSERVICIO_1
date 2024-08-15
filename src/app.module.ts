242e14744a6002be30a2ce29972427afd379fb23
Yorman <yjona@espe.edu.ec>
2024-08-15 16:19:11 -0500

    Primer commit

---
+++ src/app.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { CamisasModule } from './camisas/camisas.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    CamisasModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3307,
+      username: 'user_crud',
+      password: 'root',
+      database: 'db_crud',
+      autoLoadEntities: true,
+      synchronize: true,
+    })
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
